8c759e809ab71fde1e3b6b8eb4986c8819d9c3b9
Lensatom <ayomidetommiwa@gmail.com>
2023-01-28 07:05:26 +0100

    Initial active link indicator

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,7 +7,7 @@ const Nav = () => {
 
   return (
     <div className='w-[40%] flex justify-between items-center text-sm text-gray-500'>
-      <a href='#home' className={hash === "#home" ? 'border-b-[1px] border-b-[#DF6E1E] pb-1' : 'border-none pb-1'}> Home </a>
+      <a href='#home' className={hash === "#home" || hash === "" ? 'border-b-[1px] border-b-[#DF6E1E] pb-1' : 'border-none pb-1'}> Home </a>
       <a href='#contact' className={hash === "#contact" ? 'border-b-[1px] border-b-[#DF6E1E] pb-1' : 'border-none pb-1'}> Contact Us </a>
       <a href='#track' className={hash === "#track" ? 'border-b-[1px] border-b-[#DF6E1E] pb-1' : 'border-none pb-1'}> Track Item </a>
       <a href='/' className={'border-none pb-1'}> Login </a>
